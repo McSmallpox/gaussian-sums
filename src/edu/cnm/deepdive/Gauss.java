043f083d7e15113f43e414c468383f07a6643060
McSmallpox <ryanwands18@gmail.com>
2019-01-22 16:14:38 -0700

    add scanner and while statement

--- src/edu/cnm/deepdive/Gauss.java
+++ src/edu/cnm/deepdive/Gauss.java
@@ -1,21 +1,33 @@
 package edu.cnm.deepdive;
 
+import java.util.Scanner;
+
 public class Gauss
 {
 
   private static final int DEFAULT_UPPER_LIMIT = 100;
   public static void main(String[] args)
-  //TODO Read upper limit from args.
   {
   int upLimit = DEFAULT_UPPER_LIMIT;
-  long sum = triangleSum(upLimit);
-    System.out.printf("Sum from 1 to %,d = %,d%n", upLimit, sum); //%d and %n are placeholders. upLimit and sum fill in those placeholders
-    //in their respective orders.
+
+    Scanner scanner = new Scanner(System.in); //"Scanner" is the object type, "scanner" is the variable name. "new Scanner" means the new object is
+    //being designated as a Scanner object.
+    System.out.print("Enter upper limit: ");
+    while (scanner.hasNextInt())
+    {
+      upLimit = scanner.nextInt();
+      long sum = triangleSum(upLimit);
+      System.out.printf("Sum from 1 to %,d = %,d%n", upLimit, sum); //%d and %n are placeholders. upLimit and sum fill in those placeholders
+      //in their respective orders.
+      System.out.print("Enter upper limit: ");
+    }
+
+
   }
   public static long triangleSum(int upLimit)
   {
     long sum = 0;
-    for(int i=1; i <= upLimit; i++)
+    for (int i=1; i <= upLimit; i++)
     {
       sum+=i; //this is the same as writing sum = i + sum.
     }
